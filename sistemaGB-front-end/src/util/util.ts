bb92a64a17435c772454b0500f8337300b1e1d41
Gabriel <gloliveira@indracompany.com>
2017-04-02 00:00:39 -0300

    renomeando classe ts

---
+++ sistemaGB-front-end/src/util/util.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+//import { SelectItem } from 'primeng/primeng';
+
+
+@Injectable()
+export class Util {
+    //private selectItems: SelectItem[];
+    private pt_br: any;
+
+   /* public getListToDropDown(collection: any[], label: string): SelectItem[] {
+        this.selectItems = [];
+        this.selectItems.push({ label: 'Selecione:', value: null });
+        for (let i = 0; i < collection.length; i++) {
+            this.selectItems.push({ label: collection[i][label], value: collection[i] })
+        }
+        return this.selectItems;
+    }*/
+
+    public static getCalendarioPtBr() {
+
+        return {
+
+            firstDayOfWeek: 0,
+            dayNames: ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"],
+            dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+            dayNamesMin: ["Do", "Se", "Te", "Qa", "Qi", "Se", "Sa"],
+            monthNames: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+            monthNamesShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+        }
+    }
+
+} 
